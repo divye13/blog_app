6e80a16d7158c75c775e31dc316cf415ae86e4e8
divyesh jain <divyeshjain2005@gmail.com>
2025-06-12 21:38:34 +0530

    initial commit

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,5 @@
+class AppSecrets {
+  static const supabaseUrl = 'https://omeifoxvolmerizdahww.supabase.co';
+  static const supabaseAnonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9tZWlmb3h2b2xtZXJpemRhaHd3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk3MTkzNjYsImV4cCI6MjA2NTI5NTM2Nn0.y9b3j9e81CwOVJ14C5S7vCoVTpAN-uLaE81rKvrtQkw';
+}
